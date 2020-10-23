0cde41e08b69fcf8f25df77e0b15a3ad3ccd990e
Busra Berkson <busra.berkson@turkcell.com.tr>
2020-10-23 21:48:27 +0300

    Original file restored

--- bikeshare.py
+++ bikeshare.py
@@ -1,16 +1,3 @@
-<<<<<<< .merge_file_a08476
-<<<<<<< .merge_file_a24464
-#Code
-
-||||||| .merge_file_a18496
-=======
-##Bikershare project-1
->>>>>>> .merge_file_a05196
-||||||| .merge_file_a01516
-=======
-#Udacity program
-
->>>>>>> .merge_file_a15504
 import time
 import pandas as pd
 import numpy as np
